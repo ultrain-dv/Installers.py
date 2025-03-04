52c981a3722fdccafa731ccb0594d0841d47f2dc
Ultrain <tahjaedev@proton.me>
2025-03-04 14:28:42 +0000

    e

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ console = Console()
 title = Text("Installers.py", style="bold #90ee90", justify="center")
 
 os.system("clear")
-console.print(Panel(title, border_style="#add8e6"))
+console.print(Panel(title, border_style="#6B8E23"))
+
+console.print(Text("", style="bold #90ee90"))
 
 input()
